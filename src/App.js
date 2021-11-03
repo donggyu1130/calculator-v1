d13560345f01196aae4aedfdcef30ce778be1cda
김동규 <ehdrbehdrb27@gmail.com>
2021-11-03 23:47:58 +0900

    24번 완료

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [conclusion,setConclusion]=useState(0);
   //conclusion은 결과로 출력될 값이다
 
-  const [lastbutton,setLastbutton]=useState(0);
+  const [lastbutton,setLastbutton]=useState('NONE');
   //가장 최근에 클릭된 버튼이 저장된다
   
   /**
@@ -19,7 +19,8 @@ function App() {
   function number1Click() {
     if (lastbutton===1) {
       setConclusion(conclusion+'1');
-      setNumber(conclusion+'1');
+      setNumber(conclusion+'1');//state는 변수가 아니기 때문에 값이 즉시 바뀌지 않는다
+      setLastbutton(1);
     }else {
       setNumber(1);
       setConclusion(Number(conclusion)+1);
@@ -33,13 +34,20 @@ function App() {
    * 숫자버튼2를 클릭했을때 lastbutton에 2를 넣어준다
    */
   function number2Click() {
-    if (lastbutton===2) {
+    // console.log(lastbutton);//lastbutton값은1
+    // console.log(!lastbutton==='+');//false:!(not연산자)는 lastbutton이라는 변수의 값만 not으로 취한다
+    // console.log(lastbutton==='+');//false
+    // console.log(!(lastbutton==='+'));//true:!(not 연산자)는 바로 뒤에있는 값을 not으로 취한다
+    // console.log(lastbutton!=='+');//true:!==, !=(같지 않다 비교 연산자)
+    if (typeof lastbutton==='number') {//if를 쓰는 이유:이전에 눌렸던 버튼이 숫자였는지 확인하기 위해서이다
+      //lastbutton이 1인지 2인지 확인하는게 아니라 +가 눌렸는지 아닌지를 판별해야한다
       setConclusion(conclusion+'2');
       setNumber(conclusion+'2');
+      setLastbutton(1);
     }else {
       setNumber(2);
       setConclusion(Number(conclusion)+2);
-      setLastbutton(2);
+      setLastbutton(1);
     }
   }
 
