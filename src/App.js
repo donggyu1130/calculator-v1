232a24b6bef170e0c7296764cb9eb391fed47d43
김동규 <ehdrbehdrb27@gmail.com>
2021-11-03 22:43:51 +0900

    18번 완료

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ function App() {
   //conclusion은 결과로 출력될 값이다
 
   const [lastbutton,setLastbutton]=useState(0);
+  //가장 최근에 클릭된 버튼이 저장된다
   
   /**
    * 숫자버튼1을 클릭했을때 인풋안에 1을 출력
    * conslusion에 1을 더해준다
+   * 숫자버튼1을 클릭했을때 lastbutton에 1을 넣어준다
    */
   function number1Click() {
     if (lastbutton==1) {
@@ -25,12 +27,12 @@ function App() {
       setConclusion(conclusion+1);
       setLastbutton(1);
     }
-    
   }
 
   /**
    * 숫자버튼2를 클릭했을때 인풋안에 2를 출력
    * conslusion에 2를 더해준다
+   * 숫자버튼2를 클릭했을때 lastbutton에 2를 넣어준다
    */
   function number2Click() {
     setNumber(2);
@@ -41,22 +43,28 @@ function App() {
   /**
    * 숫자버튼3을 클릭했읋때 인풋안에 3을 출력
    * conclusion에 3을 더해준다
+   * 숫자버튼3을 클릭했을때 lastbutton에 3을 넣어준다
    */
   function number3Click() {
     setNumber(3);
     setConclusion(conclusion+3);
     setLastbutton(3);
   }
+
+  /**
+   * 연산버튼+를 클릭했을때 lastbutton에 +라는 값을 넣어준다
+   */
   function plusClick() {
     setLastbutton('+');
   }
   
   /**
    * 연산버튼=을 클릭했을때 conclusion값을 인풋안에 연산 결과로 출력
+   * 연산버튼+를 클릭했을때 lastbutton에 =이라는 값을 넣어준다
    */
   function equalClick() {
     setNumber(conclusion);
-    setLastbutton('3');
+    setLastbutton('=');
   }
 
   return (
